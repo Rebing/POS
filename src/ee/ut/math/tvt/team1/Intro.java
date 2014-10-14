ee4dd4f86d4c6265f085a609e983db5c4d73d3b5
Janno Veeorg <janno.veeorg@gmail.com>
2014-10-15 01:03:44 +0300

    Create logging

--- src/ee/ut/math/tvt/team1/Intro.java
+++ src/ee/ut/math/tvt/team1/Intro.java
@@ -3,10 +3,23 @@ package ee.ut.math.tvt.team1;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.util.Properties;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
 public class Intro {
+	private static final Logger log = Logger.getLogger(Intro.class);
+	
 	public static void main(String[] args) {
+		ConsoleAppender ca = new ConsoleAppender();
+		ca.setWriter(new OutputStreamWriter(System.out));
+		ca.setLayout(new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} %c{1} - %m%n"));
+		log.addAppender(ca);
+		
 		Properties prop = new Properties();
 		
 		//Gets the application properties file
@@ -24,5 +37,6 @@ public class Intro {
 		
 		IntroUI first = new IntroUI(prop);
 		first.init();
+		log.info("Intro window is opened");
 	}
 }
